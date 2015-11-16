a463fcd1890f3807c9e8dfb4a9967cb54838510b
Lucas <lucas@agtlucas.com>
2015-11-16 15:24:11 -0200

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,27 @@
 var path = require('path');
 var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
